1d4ff63876f9448c206d32d7911b5934116b3ad0
Ricardo Reis <ricardo.l.b.reis@gmail.com>
2019-12-04 22:54:53 -0300

    Update text-mining.r

--- text-mining.r
+++ text-mining.r
@@ -15,6 +15,7 @@
 library(tidytext)
 library(dplyr)
 library(ggplot2)
+library(wordcloud)
 
 
 # Leitura do Dados --------------------------------------------------------
@@ -29,9 +30,20 @@ tweets$text <- as.character(tweets$text)
 
 # Tokenização e Remoção de Stop Words -------------------------------------
 
+# Adicionando stop words
+custom_stop_words <- tribble(
+  ~word, ~lexicon,
+  "http", "CUSTOM",
+  "win", "CUSTOM",
+  "t.co", "CUSTOM"
+)
+
+stop_words2 <- stop_words %>% 
+  rbind(custom_stop_words)
+
 tidy_tweets <- tweets %>%
   unnest_tokens(word, text) %>%
-  anti_join(stop_words)
+  anti_join(stop_words2)
 
 contagem <- tidy_tweets %>%
   count(word) %>%
@@ -48,7 +60,13 @@ ggplot(contagem, aes(x = word2, n)) +
     x = "Palavras",
     y = "Contagem"
   )
-  ggtitle("Contagem de Palavras")
+  
+  # Criando nuvem de palavras
+  wordcloud(
+    word = contagem$word, 
+    freq = contagem$n,
+    max.words = 30
+  )
 
 
 # Análise de Sentimentos --------------------------------------------------
@@ -60,3 +78,25 @@ sentiment_tweets <- tidy_tweets %>%
 contagem_sentimento <- sentiment_tweets %>% 
   count(sentiment) %>% 
   arrange(desc(n))
+
+# Contagem de palavras por sentimento
+contagem_palavra_sentimento <- sentiment_tweets %>% 
+  count(word, sentiment) %>%
+  group_by(sentiment) %>%
+  arrange(desc(n)) %>%
+  top_n(10, n) %>%
+  ungroup() %>% 
+  mutate(word2 = fct_reorder(word, n))
+
+ggplot(contagem_palavra_sentimento, aes(x = word2, y = n, fill = sentiment)) +
+  # Don't include the lengend for the column plot
+  geom_col(show.legend = FALSE) +
+  # Facet by whether or not its a complaint and make the y-axis free
+  facet_wrap(~ sentiment, scales = "free_y") +
+  coord_flip() +
+    labs(
+      title = "Contagem de Palavras por Sentimento",
+      subtitle = "Contagem de Palavras",
+      x = "Palavras",
+      y = "Contagem"
+    )
